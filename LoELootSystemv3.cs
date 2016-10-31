42f8e9f1e6fdec6db62706f6328d84ee31f49881
Devwarlt <devwarlt@loerealm.com>
2016-10-31 20:02:22 -0200

    Updated!
    Recalculated all rates of loots.

--- LoELootSystemv3.cs
+++ LoELootSystemv3.cs
@@ -3,16 +3,16 @@
         ///////////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
         /////////////////////////////////////////New Loot System in LoE Realm by Devwarlt\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
         ///////////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-        private const double whitebag = 0.00728*2*eventmultipler;         //0.728%        -       White Bag
-        private const double blackbag = 0.00222*2*eventmultipler;         //0.222%        -       Black Bag LT
-        private const double superbag = 0.00182*2*eventmultipler;         //0.182%        -       Super Bag SRT
-        private const double eggbag = 0.0040625*2*eventmultipler;         //0.40625%      -       Egg Bag
-        private const double awesomeloot = 0.008125*2*eventmultipler;     //0.8125%       -       T13~14 weapons / T14~15 armors / T7~8 abilities / T7~8 rings / treasures
-        private const double greatloot = 0.01625*2*eventmultipler;        //1.625%        -       T12 weapons / T13 armors / T6 abilities / T6 rings
-        private const double goodloot = 0.03125*2*eventmultipler;         //3.125%        -       T10-11 weapons / T12 armors / T5 abilities / T5 rings
-        private const double normalloot = 0.0625*2*eventmultipler;        //6.25%         -       T9 weapons / T10-11 armors / T4 abilities / T4 rings
-        private const double mediumloot = 0.125*2*eventmultipler;         //12.5%         -       T7-8 weapons / T8-9 armors / T3 abilities /T3 rings
-        private const double poorloot = 0.20*2*eventmultipler;            //20%           -       T0-6 weapons / T1-7 armors / T0-2 abilities / T1-2 rings
+        private const double whitebag = 0.01456*eventmultipler;           //1.456%        -       White Bag UT
+        private const double blackbag = 0.00444*eventmultipler;           //0.444%        -       Black Bag LT
+        private const double superbag = 0.364*eventmultipler;             //0.364%        -       Super Bag SRT
+        private const double eggbag = 0.008125*eventmultipler;            //0.8125%      -       Egg Bag
+        private const double awesomeloot = 0.01625*eventmultipler;        //1.625%       -       T13~14 weapons / T14~15 armors / T7~8 abilities / T7~8 rings / treasures
+        private const double greatloot = 0.03125*eventmultipler;          //3.125%        -       T12 weapons / T13 armors / T6 abilities / T6 rings
+        private const double goodloot = 0.0625*eventmultipler;            //6.25%         -       T10-11 weapons / T12 armors / T5 abilities / T5 rings
+        private const double normalloot = 0.125*eventmultipler;           //12.5%        -       T9 weapons / T10-11 armors / T4 abilities / T4 rings
+        private const double mediumloot = 0.25*eventmultipler;            //25%         -       T7-8 weapons / T8-9 armors / T3 abilities /T3 rings
+        private const double poorloot = 0.4*eventmultipler;               //40%           -       T0-6 weapons / T1-7 armors / T0-2 abilities / T1-2 rings
         ///////////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
         ///////////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
         ///////////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
